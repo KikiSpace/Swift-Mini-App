65075be5491b3131a42f9db591d5792db0994ad9
KikiSpace <xzhangkiki@gmail.com>
2024-02-07 13:12:27 -0800

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Xinqi Zhang on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
